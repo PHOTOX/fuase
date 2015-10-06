244800a5f4d8324be052143e5edace8cc2dd7740
Frank Uhlig <fuhlig@tahr.icp.uni-stuttgart.de>
2015-10-06 14:10:55 +0200

    fixed atoms.patch and generated patch script

---
+++ patch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OWD=`pwd`
+ASEDIR=/scratch/fuhlig/opt/ase/trunk/ase
+
+cp calculators/{gebf.py,orca.py,nwchem.py,gamess_us.py} ${ASEDIR}/calculators/
+cp io/qxyz.py ${ASEDIR}/io/
+
+cd ${ASEDIR}
+patch -p0 <${OWD}/atoms.patch
+
+cd ${OWD}
+
+#to clean up the just messed up repository go to $ASEDIR and run:
+#   svn revert --recursive ./
+#in the top level ase directory. 
+#Be aware that this will revert all changes that have not been commited, also those that have not been done by this file.
